10269c38caa3be13fda9d8fe466936d097115cca
DrissiOmar <omar.drissi@esprit.tn>
2025-04-05 22:36:39 +0100

    Initial commit: Project generated via Spring Initializr

---
+++ Server/restaurant-review-platform/src/main/java/com/omar/restaurant_review_platform/RestaurantReviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.omar.restaurant_review_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReviewPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReviewPlatformApplication.class, args);
+	}
+
+}
---
+++ Server/restaurant-review-platform/src/test/java/com/omar/restaurant_review_platform/RestaurantReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.restaurant_review_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
